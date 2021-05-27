628a043c5873933482e31fdedda0d3e1aae3693c
Ayisha <ayisha.gts@emaar.ae>
2021-05-27 16:56:04 +0400

    Initial Commit - Added Sign In Page

---
+++ lib/common_widgets/custom_elevated_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class CustomElevatedButton extends StatelessWidget {
+  CustomElevatedButton({
+    this.child,
+    this.color,
+    this.borderRadius: 2.0,
+    this.height: 50.0,
+    this.onPressed,
+  });
+
+  final Widget child;
+  final Color color;
+  final double borderRadius;
+  final double height;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(color),
+          shape: MaterialStateProperty.all(RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(borderRadius),
+            ),
+          )),
+        ),
+        child: child,
+        onPressed: onPressed,
+      ),
+    );
+  }
+}
